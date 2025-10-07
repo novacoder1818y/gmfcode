a2706ada6caa4a6905b1bb16e4b94197d00788d4
himanshujadav77 <himanshujadav1877@gmail.com>
2025-10-07 22:12:02 +0530

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 // lib/main.dart
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:gmfcode/firebase_options.dart';
 
 import 'app/routes/app_pages.dart';
 import 'app/theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
